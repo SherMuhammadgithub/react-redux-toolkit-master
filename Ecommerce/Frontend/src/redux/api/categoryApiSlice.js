6cd6d7d115c056d103ee3355898e5aff46656805
SherMuhammadgithub <muhammadiqbalshermuhammad@gmail.com>
2024-06-02 11:01:52 +0500

    Add category URL constant to redux/constants.js

---
+++ Ecommerce/Frontend/src/redux/api/categoryApiSlice.js
@@ -0,0 +1,40 @@
+import { CATEGORY_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const categoryApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    createCategory: builder.mutation({
+      query: (data) => ({
+        url: `${CATEGORY_URL}`,
+        method: "POST",
+        body: data,
+      }),
+    }),
+    updateCategory: builder.mutation({
+      query: (updatedCategory, categoryId) => ({
+        url: `${CATEGORY_URL}/${categoryId}`,
+        method: "PUT",
+        body: updatedCategory,
+      }),
+    }),
+    deleteCategory: builder.mutation({
+      query: (categoryId) => ({
+        url: `${CATEGORY_URL}/${categoryId}`,
+        method: "DELETE",
+      }),
+    }),
+    fetchCategories: builder.query({
+      query: () => ({
+        url: `${CATEGORY_URL}/categories`,
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const {
+  useCreateCategoryMutation,
+  useUpdateCategoryMutation,
+  useDeleteCategoryMutation,
+  useFetchCategoriesQuery,
+} = categoryApiSlice;
