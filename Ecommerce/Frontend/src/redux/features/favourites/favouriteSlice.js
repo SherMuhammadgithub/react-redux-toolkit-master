e463858e049e38a3eccff18a36eca6475d7e2a64
SherMuhammadgithub <muhammadiqbalshermuhammad@gmail.com>
2024-06-06 09:57:04 +0500

    Refactor favouriteSlice.js in Ecommerce Frontend

--- Ecommerce/Frontend/src/redux/features/favourites/favouriteSlice.js
+++ Ecommerce/Frontend/src/redux/features/favourites/favouriteSlice.js
@@ -1,1 +1,26 @@
-import {createSlice}
+import { createSlice } from "@reduxjs/toolkit";
+
+const favouriteSlice = createSlice({
+  name: "favourites",
+  intialState: [],
+  reducers: {
+    addToFavourites: (state, action) => {
+      // check is already in favourites
+      if (!state.some((product) => product._id === action.payload._id)) {
+        state.push(action.payload);
+      }
+    },
+    removeFromFavourites: (state, action) => {
+      return state.filter((product) => product._id !== action.payload._id);
+    },
+    setFavorites: (state, action) => {
+      return action.payload;
+    },
+  },
+});
+
+export const { addToFavourites, removeFromFavourites, setFavorites } =
+  favouriteSlice.actions;
+
+export const selectFavourites = (state) => state.favourites; // it will be used in the component to get the favourites from the store
+export default favouriteSlice.reducer;
